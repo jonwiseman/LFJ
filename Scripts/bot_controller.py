326b9841f3ef50a62facd89d84024b43a96e8622
wisemanj <wisemanj@etown.edu>
2020-03-04 19:37:29 -0500

    Comments and updated requirements.txt

--- Scripts/bot_controller.py
+++ Scripts/bot_controller.py
@@ -4,54 +4,71 @@ import user_queries
 
 
 def main():
-    commands = {
+    commands = {        # list of commands available for bot to use
         'add_user': user_queries.main,
         'query_user': user_queries.main,
         'delete_user': user_queries.main,
         'update_user': user_queries.main
     }
 
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser()        # read and parse configuration file
     config.read(r'..\configuration.conf')
 
-    token = config['Discord']['token']
+    token = config['Discord']['token']      # get the bot's unique token for sign-in
 
-    client = discord.Client()
+    client = discord.Client()       # create the bot client
 
     @client.event
     async def on_ready():
+        """
+        on_ready() is called when the bot is signed in to Discord and ready to send/receive event notifications
+        :return: none; print ready status to console
+        """
         print('We have logged in as {0.user}'.format(client))
 
     @client.event
     async def on_message(message):
-        if message.author == client.user:
+        """
+        on_message() is called when a message is sent in any channel that the bot is part of.  This includes messages
+        sent from the bot itself.
+        :param message: the message object that was sent
+        :return: the bot will parse the message; there are two options: a response message is sent or no message is sent
+        """
+        if message.author == client.user:       # if the bot sent the message, then no action is taken
             return
-        if message.content.startswith('exit'):
-            await client.logout()
-            return
-        if not check_mentions(message.mentions[0], client):
+        if message.content.startswith('exit'):      # received the exit command
+            await client.logout()       # log the bot out
+            return      # return and end the program
+        if not check_mentions(message.mentions[0], client):     # if the bot was not mentioned, do nothing
             return
 
         tokens = message.content.split(' ')[1:]     # get tokens
         command = tokens[0]     # according to syntax, next token is the command
 
-        if command not in commands:
+        if command not in commands:     # if the command is not recognized, then notify the sender
             await message.channel.send("Invalid command")
             return
 
-        tokens.append(message.author)
-        result = commands[command](len(tokens), tokens)
+        tokens.append(message.author)       # get the sending user's information and pass it as an argument
+        result = commands[command](len(tokens), tokens)     # handle the command according to command dictionary
 
-        if result == 1:
+        if result == 1:     # if there was an error executing the command, notify the user
             await message.channel.send("Error executing command.  Please consult syntax")
             return
 
-        await message.channel.send(result)
+        await message.channel.send(result)      # if there were no errors, then send the appropriate response
 
-    client.run(token)
+    client.run(token)       # start the bot based on its sign-in token
 
 
 def check_mentions(first_mention, client):
+    """
+    Check the users mentioned in a message to see if @LFJ is included.  Returns True only if the user was the first
+    mention in the message.
+    :param first_mention: the user object corresponding to the first mention in the message
+    :param client: the client object (i.e. the bot); for checking against first_mention
+    :return: True if the first mention is the bot; False otherwise
+    """
     return str(first_mention) == str(client.user)
 
 
--- Scripts/user_queries.py
+++ Scripts/user_queries.py
@@ -3,52 +3,59 @@ import mysql.connector
 
 
 def main(argc, argv):
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser()        # read and parse the config file
     config.read(r'..\configuration.conf')
 
-    username = config['Database']['username']
+    username = config['Database']['username']       # get details for signing in to database
     password = config['Database']['password']
     host = config['Database']['host']
     database = config['Database']['database']
 
-    try:
+    try:        # try connecting to the database
         cnx = mysql.connector.connect(user=username,
                                       password=password,
                                       host=host,
                                       database=database)
-    except mysql.connector.Error:
+    except mysql.connector.Error:       # catch connection errors
         return 1
-    else:
+    else:       # if successful in connecting, create a cursor object
         cursor = cnx.cursor()
 
-    command = argv[0]
+    command = argv[0]       # get the command entered by the user
     if command == 'query_user':
-        if argc > 3:
+        if argc > 3:        # syntax dictates 2 arguments (command, ALL/USER); third argument is the user who requested
             return 1
         return query_user(argv[1], cursor)
     elif command == 'add_user':
-        if argc != 5:
+        if argc != 5:       # syntax dictates 4 arguments (command, user id, display name, admin status)
             return 1
-        data_insert = {
-            'user_id': argv[1].split('#')[1],
-            'display_name': argv[1],
-            'e_mail': argv[2],
-            'admin': argv[3]
+        data_insert = {     # prepare data for insertion into database
+            'user_id': argv[1].split('#')[1],       # get the integer ID from the display name
+            'display_name': argv[1],        # display name passed by user
+            'e_mail': argv[2],      # e-mail entered by user
+            'admin': argv[3]        # admin status entered by user
         }
         return add_user(str(argv[-1]), data_insert, cursor, cnx)
     elif command == 'delete_user':
-        if argc != 3:
+        if argc != 3:       # syntax dictates 2 arguments (command, display name)
             return 1
         return delete_user(str(argv[-1]), argv[1], cursor, cnx)
     elif command == 'update_user':
-        if argc != 4:
+        if argc != 4:       # syntax dictates 3 arguments (command, display name, new email)
             return 1
         return update_user(str(argv[-1]), argv[1], argv[2], cursor, cnx)
 
-    cnx.close()
+    cnx.close()     # close the connection to the database
 
 
 def query_user(argument, cursor):
+    """
+    Query information from the user table.
+    :param argument: either ALL (for a select * from user query) or a display name (for getting information about a
+    specific user).
+    :param cursor: cursor to execute queries
+    :return: The result of the select statement
+    """
     if argument == 'ALL':
         cursor.execute('select * from user')
         return cursor.fetchall()
@@ -58,48 +65,79 @@ def query_user(argument, cursor):
 
 
 def delete_user(auth_user, display_name, cursor, cnx):
-    admin_status = check_admin_status(auth_user, cursor)
+    """
+    Delete a row from the user table based on display name.
+    :param auth_user: user who is authorizing the delete
+    :param display_name: display name of the user to be deleted
+    :param cursor: cursor object for executing query
+    :param cnx: connection object for committing changes
+    :return: the new table after deletion or an error flag
+    """
+    admin_status = check_admin_status(auth_user, cursor)        # see if the authorizing user is an admin
     if admin_status == -1 or admin_status == 0:  # authorizing user does not exist or does not have permission
         return 1
 
-    delete_admin = check_admin_status(display_name, cursor)
+    delete_admin = check_admin_status(display_name, cursor)     # see if the user to be deleted is an admin
     if delete_admin == -1 or delete_admin == 1:     # trying to delete a non-existant user or another admin
         return 1
 
-    cursor.execute('delete from user where display_name = %s', (display_name,))
-    cnx.commit()
-    cursor.execute('select * from user')
+    cursor.execute('delete from user where display_name = %s', (display_name,))     # execute deletion query
+    cnx.commit()        # commit changes to database
+    cursor.execute('select * from user')        # get new user table
     return cursor.fetchall()
 
 
 def add_user(auth_user, data_insert, cursor, cnx):
-    admin_status = check_admin_status(auth_user, cursor)
+    """
+    Add a user to the user table.
+    :param auth_user: user authorizing add
+    :param data_insert: dictionary of data to be inserted (user id, display name, email, and admin status)
+    :param cursor: cursor for executing query
+    :param cnx: connection object for committing change
+    :return: the new table after insertion or an error flag
+    """
+    admin_status = check_admin_status(auth_user, cursor)        # see if the authorizing user is an admin
     if admin_status == -1 or admin_status == 0:      # authorizing user does not exist or does not have permission
         return 1
 
     cursor.execute('insert into user '
                    '(user_id, display_name, e_mail, admin) '
-                   'values (%(user_id)s, %(display_name)s, %(e_mail)s, %(admin)s)', data_insert)
-    cnx.commit()
+                   'values (%(user_id)s, %(display_name)s, %(e_mail)s, %(admin)s)', data_insert)        # add new user
+    cnx.commit()        # commit changes to database
 
-    cursor.execute('select * from user')
+    cursor.execute('select * from user')        # get new user table
     return cursor.fetchall()
 
 
 def update_user(auth_user, display_name, email, cursor, cnx):
-    admin_status = check_admin_status(auth_user, cursor)
+    """
+    Update the email associated with a user.
+    :param auth_user: user authorizing change
+    :param display_name: display name of user whose email will be changed
+    :param email: new email for user
+    :param cursor: cursor object to execute query
+    :param cnx: connection object to commit changes
+    :return: the new table after updating the user table
+    """
+    admin_status = check_admin_status(auth_user, cursor)        # see if the authorizing user is an admin
     if admin_status == -1 or admin_status == 0:  # authorizing user does not exist or does not have permission
         return 1
 
     cursor.execute('update user '
                    'set e_mail = %s '
-                   'where display_name = %s', (email, display_name))
-    cnx.commit()
-    cursor.execute('select * from user')
+                   'where display_name = %s', (email, display_name))        # change the user table with new email
+    cnx.commit()        # commit changes to user table
+    cursor.execute('select * from user')        # get new user table
     return cursor.fetchall()
 
 
 def check_admin_status(display_name, cursor):
+    """
+    Check to see if a given user is an admin.  Only admins can change the database.
+    :param display_name: display name of requesting user
+    :param cursor: cursor object for executing search query
+    :return: 0 if the user is not an admin (or does not exist) or 1 if the user is an admin
+    """
     cursor.execute('select admin from user where display_name = %s', (display_name,))
     result = cursor.fetchall()
 
